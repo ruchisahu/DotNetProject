b6ba7dc61e78d7d5ea9cd03ef3f375cae0a570cf
ruchisahu <sahruchi@gmail.com>
2019-06-17 12:03:20 -0700

    added Task status

---
+++ TaskStatus/TaskStatus/Models/DBModels.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace TaskStatus.Models
+{
+    public class DBModels: DbContext
+    {
+        public DBModels()
+            : base("name=TestDatabase")
+        { }
+        
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+        public virtual DbSet<Task> Task { get; set; }
+    }
+}
